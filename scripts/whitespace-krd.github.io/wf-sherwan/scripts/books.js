6dd707f8710b94008834ef531f6362b69e439db4
Teki <iiiklox@gmail.com>
2024-03-14 13:52:13 +0300

    fix a ton of shit :D

---
+++ scripts/whitespace-krd.github.io/wf-sherwan/scripts/books.js
@@ -0,0 +1,416 @@
+const books = {
+  "2019": {
+    "books": [
+      {
+        "name": "1984",
+        "path": "assets/book-covers/2019-1984-George Orwell.jpg"
+      },
+      {
+        "name": "Alchemist",
+        "path": "assets/book-covers/2019-Alchemist-Paulo Coelho.jpg"
+      },
+      {
+        "name": "All my sons",
+        "path": "assets/book-covers/2019-All my sons-Arthur Miller.jpg"
+      },
+      {
+        "name": "Animal Farm",
+        "path": "assets/book-covers/2019-Animal Farm-George Orwell.jpg"
+      },
+      {
+        "name": "Hamlet",
+        "path": "assets/book-covers/2019-Hamlet-William Shakespeare.jpg"
+      },
+      {
+        "name": "Invisible Women",
+        "path": "assets/book-covers/2019-Invisible Women-Caroline Perez.jpg"
+      },
+      {
+        "name": "Mastery",
+        "path": "assets/book-covers/2019-Mastery-Robert Greene.jpg"
+      },
+      {
+        "name": "Men without Women",
+        "path": "assets/book-covers/2019-Men without Women-Haruki.jpg"
+      },
+      {
+        "name": "The Da Vinci Code",
+        "path": "assets/book-covers/2019-The Da Vinci Code-Dan Brown.jpg"
+      },
+      {
+        "name": "The Kite Runner",
+        "path": "assets/book-covers/2019-The Kite Runner-Khaled Hosseini.jpg"
+      },
+      {
+        "name": "The Spy",
+        "path": "assets/book-covers/2019-The Spy-Paulo Coelho.jpg"
+      },
+      {
+        "name": "Think and grow Rich",
+        "path": "assets/book-covers/2019-Think and grow Rich-Napoleon Hill.jpg"
+      },
+      {
+        "name": "To kill a Mockingbird",
+        "path": "assets/book-covers/2019-To kill a Mockingbird-Harper Lee.jpg"
+      },
+      {
+        "name": "Who says you can\u2019t",
+        "path": "assets/book-covers/2019-Who says you can\u2019t-Daniel Chidiac.jpg"
+      }
+    ]
+  },
+  "2020": {
+    "books": [
+      {
+        "name": "Angels & Demons",
+        "path": "assets/book-covers/2020-Angels & Demons-Dan Brown.jpg"
+      },
+      {
+        "name": "City of Thieves",
+        "path": "assets/book-covers/2020-City of Thieves-David Benioff.jpg"
+      },
+      {
+        "name": "Da Vinci",
+        "path": "assets/book-covers/2020-Da Vinci-Walter Isaacson.jpg"
+      },
+      {
+        "name": "Elon Musk",
+        "path": "assets/book-covers/2020-Elon Musk-Ashlee Vance.jpg"
+      },
+      {
+        "name": "End of Days",
+        "path": "assets/book-covers/2020-End of Days-Sylvia Browne.jpg"
+      },
+      {
+        "name": "Exile & the kingdom",
+        "path": "assets/book-covers/2020-Exile & the kingdom-Albert Camus.jpg"
+      },
+      {
+        "name": "Journey of Souls",
+        "path": "assets/book-covers/2020-Journey of Souls-Michael Newton.jpg"
+      },
+      {
+        "name": "Notes from underground",
+        "path": "assets/book-covers/2020-Notes from underground-Fyodor.jpg"
+      },
+      {
+        "name": "richest man babylon",
+        "path": "assets/book-covers/2020-richest man babylon-george clason.jpg"
+      },
+      {
+        "name": "Sapiens",
+        "path": "assets/book-covers/2020-Sapiens-Yuval Noah Harari.jpg"
+      },
+      {
+        "name": "Small Data",
+        "path": "assets/book-covers/2020-Small Data-Martin Lindstrom.jpg"
+      },
+      {
+        "name": "The Fall",
+        "path": "assets/book-covers/2020-The Fall-Albert Camus.jpg"
+      },
+      {
+        "name": "The Human Body",
+        "path": "assets/book-covers/2020-The Human Body-Tom Jackson.jpg"
+      },
+      {
+        "name": "the Myth of sisyphus",
+        "path": "assets/book-covers/2020-the Myth of sisyphus-Albert Camus.jpg"
+      },
+      {
+        "name": "The Plague",
+        "path": "assets/book-covers/2020-The Plague-Albert Camus.jpg"
+      },
+      {
+        "name": "The Prophet",
+        "path": "assets/book-covers/2020-The Prophet-Kahlil Gibran.jpg"
+      }
+    ]
+  },
+  "2021": {
+    "books": [
+      {
+        "name": "Albert camu writings",
+        "path": "assets/book-covers/2021-Albert camu writings-albert camus.jpg"
+      },
+      {
+        "name": "Caligula & other play",
+        "path": "assets/book-covers/2021-Caligula & other play-albert camus.jpg"
+      },
+      {
+        "name": "down&out in P&L",
+        "path": "assets/book-covers/2021-down&out in P&L-George Orwell.jpg"
+      },
+      {
+        "name": "Homo Deus",
+        "path": "assets/book-covers/2021-Homo Deus-Yuval Noah Harari.jpg"
+      },
+      {
+        "name": "the catcher in the rye",
+        "path": "assets/book-covers/2021-the catcher in the rye-J.D. salinger.jpg"
+      },
+      {
+        "name": "The death of ivan ilyich",
+        "path": "assets/book-covers/2021-The death of ivan ilyich-Leo Tolstoy.jpg"
+      },
+      {
+        "name": "The Schopenhauer cure",
+        "path": "assets/book-covers/2021-The Schopenhauer cure-Irvin yalom.jpg"
+      },
+      {
+        "name": "The Selfish Gene",
+        "path": "assets/book-covers/2021-The Selfish Gene-Richard Dawkins.jpg"
+      },
+      {
+        "name": "When nietzsche wept",
+        "path": "assets/book-covers/2021-When nietzsche wept-Irvin Yalom.jpg"
+      },
+      {
+        "name": "Why I write",
+        "path": "assets/book-covers/2021-Why I write-George Orwell.jpg"
+      },
+      {
+        "name": "Why we sleep",
+        "path": "assets/book-covers/2021-Why we sleep-Matthew Walker.jpg"
+      }
+    ]
+  },
+  "2022": {
+    "books": [
+      {
+        "name": "A brief history of time",
+        "path": "assets/book-covers/2022-A brief history of time-S. Hawking.jpg"
+      },
+      {
+        "name": "Clinton Cash",
+        "path": "assets/book-covers/2022-Clinton Cash-Peter Schweizer.jpg"
+      },
+      {
+        "name": "Create dangerously",
+        "path": "assets/book-covers/2022-Create dangerously-Albert camus.jpg"
+      },
+      {
+        "name": "Daughters of Kobani",
+        "path": "assets/book-covers/2022-Daughters of Kobani-Gayle lemmon.jpg"
+      },
+      {
+        "name": "Life 3.0",
+        "path": "assets/book-covers/2022-Life 3.0-Max Tegmark.jpg"
+      },
+      {
+        "name": "lyrical & critical essay",
+        "path": "assets/book-covers/2022-lyrical & critical essay-albert camus.jpg"
+      },
+      {
+        "name": "Nausea",
+        "path": "assets/book-covers/2022-Nausea-Jean Paul Sartre.jpg"
+      },
+      {
+        "name": "No Exit",
+        "path": "assets/book-covers/2022-No Exit-Jean Paul Sartre.jpg"
+      },
+      {
+        "name": "Outwitting the devil",
+        "path": "assets/book-covers/2022-Outwitting the devil-Napoleon Hill.jpg"
+      },
+      {
+        "name": "Principles of home",
+        "path": "assets/book-covers/2022-Principles of home-kevin McCloud.jpg"
+      },
+      {
+        "name": "Resistance.. & death",
+        "path": "assets/book-covers/2022-Resistance.. & death-albert camus.jpg"
+      },
+      {
+        "name": "The big picture",
+        "path": "assets/book-covers/2022-The big picture-Sean Carroll.jpg"
+      },
+      {
+        "name": "The denial of death",
+        "path": "assets/book-covers/2022-The denial of death-Ernest becker.jpg"
+      },
+      {
+        "name": "The orange girl",
+        "path": "assets/book-covers/2022-The orange girl-Jostein Gaarder.jpg"
+      },
+      {
+        "name": "The Pale Horse",
+        "path": "assets/book-covers/2022-The Pale Horse-Agatha Christie.jpg"
+      },
+      {
+        "name": "what i talk about",
+        "path": "assets/book-covers/2022-what i talk about-Haruki Murakami.jpg"
+      }
+    ]
+  },
+  "2023": {
+    "books": [
+      {
+        "name": "As a man Thinketh",
+        "path": "assets/book-covers/2023-As a man Thinketh-James Allen.jpg"
+      },
+      {
+        "name": "Be Exceptional",
+        "path": "assets/book-covers/2023-Be Exceptional-Joe Navarro.jpg"
+      },
+      {
+        "name": "becoming supernatural",
+        "path": "assets/book-covers/2023-becoming supernatural-joe dispenza.jpg"
+      },
+      {
+        "name": "Grand design abroad",
+        "path": "assets/book-covers/2023-Grand design abroad-kevin McCloud.jpg"
+      },
+      {
+        "name": "Meditations",
+        "path": "assets/book-covers/2023-Meditations-Marcus Aurelius.jpg"
+      },
+      {
+        "name": "Pride and Prejudice",
+        "path": "assets/book-covers/2023-Pride and Prejudice-Jane Austen.jpg"
+      },
+      {
+        "name": "Rich dad poor dad",
+        "path": "assets/book-covers/2023-Rich dad poor dad-Robert kiyosaki.jpg"
+      },
+      {
+        "name": "Spare",
+        "path": "assets/book-covers/2023-Spare-Prince Harry.jpg"
+      },
+      {
+        "name": "The Blind Owl",
+        "path": "assets/book-covers/2023-The Blind Owl-Sadegh Hedayat.jpg"
+      },
+      {
+        "name": "The Words",
+        "path": "assets/book-covers/2023-The Words-Jean Paul Sartre.jpg"
+      },
+      {
+        "name": "thus spoke Zarathustra",
+        "path": "assets/book-covers/2023-thus spoke Zarathustra-Nietzsche.jpg"
+      },
+      {
+        "name": "To have or to be",
+        "path": "assets/book-covers/2023-To have or to be-Erich Fromm.jpg"
+      },
+      {
+        "name": "Wit & wisdom",
+        "path": "assets/book-covers/2023-Wit & wisdom-Oscar wilde.jpg"
+      }
+    ]
+  }
+}
+
+
+// add all of those images to the DOM
+
+// template: <div class="books"><div class="2019"><img src="assets/book-covers/2019-1984-George Orwell.jpg" alt="1984" /><img src="assets/book-covers/2019-1984-George Orwell.jpg" alt="1984" </div></div>
+
+
+// add all of those images to the DOM
+const booksContainer = document.querySelector('.books');
+const aspectRatio = 1.2;
+
+for (const year in books) {
+  const yearDiv = document.createElement('div');
+  yearDiv.classList.add(`year-${year}`); // Prefix the year with "year-"
+  yearDiv.style.height = '100%';
+  yearDiv.style.width = '100%';
+  yearDiv.style.display = 'flex';
+  yearDiv.style.position = 'absolute';
+  yearDiv.style.justifyContent = 'center';
+  yearDiv.style.alignItems = 'center';
+  yearDiv.style.opacity = '0%'; // Initially hide all year divs
+
+  for (const book of books[year].books) {
+    const img = document.createElement('div');
+    img.style.backgroundImage = `url("https://whitespace-krd.github.io/wf-sherwan/${book.path}")`;
+    img.alt = book.name;
+    img.style.width = `calc(90% / ${aspectRatio})`;
+    img.style.height = `90%`;
+    img.style.position = 'absolute';
+    img.style.border = '5px solid white';
+    img.style.backgroundSize = 'cover';
+    img.style.backgroundPosition = 'center';
+    img.style.backgroundRepeat = 'no-repeat';
+
+    yearDiv.appendChild(img);
+  }
+
+  booksContainer.appendChild(yearDiv);
+}
+
+let tl; // Declare the timeline variable outside the function
+
+// Function to create and start the animation timeline
+function startAnimation(yearDiv) {
+  const bookElements = yearDiv.querySelectorAll('div');
+  const totalBooks = bookElements.length;
+
+  // Stop any existing timeline
+  if (tl) {
+    tl.kill();
+  }
+
+  let globalIndex = 10;
+
+  // Create a new GSAP timeline
+  tl = gsap.timeline({ repeat: -1, delay: 4.6 });
+
+  // Loop over each book element
+  bookElements.forEach((book, index) => {
+    // Calculate the z-index based on the desired order
+    const zIndex = globalIndex + index;
+    book.style.zIndex = zIndex;
+
+    // Store the initial position and rotation of the book
+    const initialX = getComputedStyle(book).getPropertyValue('x');
+    const initialRotation = getComputedStyle(book).getPropertyValue('rotation');
+
+    // Add a tween for each book to the timeline
+    const bookTween = gsap.timeline()
+      .to(book, { x: '0%', duration: 0, ease: "expo" })
+      .to(book, { x: '-50%', rotation: -20, duration: 0.5, ease: "expo" }, index * 5)
+      .call(() => {
+        // Update the z-index of the current book to be on top
+        book.style.zIndex = globalIndex + totalBooks;
+      })
+      .to(book, { x: '0%', rotation: 0, duration: 1, ease: "expo" })
+      .to(book, { x: initialX, rotation: initialRotation, duration: 0 }); // Reset position and rotation
+
+    tl.add(bookTween, index * 5); // Add the book tween to the main timeline
+  });
+}
+gsap.registerPlugin(CustomEase) 
+// Get all the year buttons
+const yearButtons = document.querySelectorAll('.btn-year');
+CustomEase.create("bouncy", "1, -.356, 0, 1");
+// Add event listeners to each button
+yearButtons.forEach(button => {
+  // Hover animation
+  button.addEventListener('mouseenter', () => {
+    gsap.to(button, { scale: 1.2, duration: 0.3, ease: "power2"});
+  });
+
+  button.addEventListener('mouseleave', () => {
+    gsap.to(button, { scale: 1, duration: 0.3, ease: "power2" });
+  });
+
+  // Click event to change the year
+  button.addEventListener('click', () => {
+    const selectedYear = button.id; // Get the year from the button's id
+
+    // Hide all year divs
+    const yearDivs = document.querySelectorAll('.books > div');
+    yearDivs.forEach(div => {
+      gsap.to(div, { translateY: '-5%', opacity: 0, duration: 0.4, ease: "bouncy" });
+    });
+
+    // Show the selected year div
+    const selectedYearDiv = document.querySelector(`.year-${selectedYear}`);
+    gsap.to(selectedYearDiv, { translateY: '0%', opacity: 1, duration: 0.4, ease: "bouncy" });
+
+    // Start the animation for the selected year
+    startAnimation(selectedYearDiv);
+  });
+});
